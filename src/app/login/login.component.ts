45bf2a31d8816745bfb9c339506a77e95e36b555
Mohammad EL AZZAOUI <mohammadelazzaoui1999@gmail.com>
2024-04-17 00:47:38 +0200

    4/16

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ClientService } from '../services/client.service';
- 
+
 
 @Component({
   selector: 'app-login',
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  
+
   private loggedIn = new BehaviorSubject<boolean>(false);
 
   constructor() { }
